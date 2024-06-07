b40e3fc78265302a1fc13c67f8f2342eef415a0d
henryk86 <166761451+henryk86@users.noreply.github.com>
2024-06-07 21:59:43 +0200

    extend to kind of external json

--- src/pages/tonies/FreeContent.tsx
+++ src/pages/tonies/FreeContent.tsx
@@ -1,5 +1,5 @@
-import { useTranslation } from "react-i18next";
-import { Card, List, Tooltip, Typography, theme } from "antd";
+import { Trans, useTranslation } from "react-i18next";
+import { Button, Card, DatePicker, Form, Input, List, Modal, Tooltip, Typography, theme } from "antd";
 
 import {
     HiddenDesktop,
@@ -10,69 +10,159 @@ import {
 } from "../../components/StyledComponents";
 import { ToniesSubNav } from "../../components/tonies/ToniesSubNav";
 import { Link } from "react-router-dom";
-import { LinkOutlined } from "@ant-design/icons";
+import { LinkOutlined, PlusOutlined } from "@ant-design/icons";
 import LanguageFlagSVG from "../../utils/languageUtil";
+import { useEffect, useState } from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { oneDark, oneLight } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const { Paragraph } = Typography;
 const { Text } = Typography;
 const { useToken } = theme;
 
+interface SourceData {
+    sourceTitle: string;
+    title: string;
+    description?: string;
+    source: string;
+    image: string;
+    language: string;
+    dateOfAdding?: string;
+}
+
 export const FreeContentPage = () => {
     const { t } = useTranslation();
     const { token } = useToken();
 
-    const sourceArray = [
-        {
-            sourceTitle: "ohrka.de",
-            title: "Das hochwertige Hörportal für Kinder",
-            description:
-                "Bekannte und beliebte Stimmen wie Anke Engelke oder Katharina Thalbach lesen Hörspiele und Hörbücher für Kinder. Über 80 Stunden anhören. Kostenlos, werbefrei. Auch zum downloaden. Pädi-Gütesiegel 2013. Spannende, lustige und vor allem kostenlose Kinderhörspiele zum Hören und als mp3-Datei zum Download.",
-            source: "https://www.ohrka.de",
-            image: "https://www.ohrka.de/templates/public/img/logo.png",
-            language: "de-de",
-            dateOfAdding: "2024-06-07",
-        },
-        {
-            sourceTitle: "Vorleser.net",
-            title: "Kostenlose mp3-Hörbücher und Hörspiele",
-            description:
-                "750+ kostenlose mp3-Hörbücher und Hörspiele legal als Stream oder zum Download! Von professionellen Sprechern exklusiv für vorleser.net produzierte Hörbücher und Hörspiele",
-            source: "https://www.vorleser.net",
-            image: "https://www.vorleser.net/resources/images/favicon/android-chrome-192x192.png",
-            language: "de-de",
-            dateOfAdding: "2024-06-07",
-        },
-        {
-            sourceTitle: "Gratis-Hoerspiele.de",
-            title: "Gratis Hörspiele & Hörbücher | Kostenlos und legal",
-            description:
-                "Über 3.000 kostenlose Hörspiele und Hörbücher. Legal und gratis als MP3 runterladen oder streamen! Entdecke täglich neue kostenfreie Downloads und Streams.",
-            source: "https://www.gratis-hoerspiele.de",
-            image: "https://www.gratis-hoerspiele.de/wp-content/uploads/2018/02/cropped-symbol-270x270.png",
-            language: "de-de",
-            dateOfAdding: "2024-06-07",
-        },
-        {
-            sourceTitle: "Hörbuch Kostenlos 2024",
-            title: "Hörbücher kostenlose hören oder herunterladen",
-            description:
-                "Kostenlose Hörbuch-Sammlung von mehr als 200.000 heutzutage beliebtesten Titeln von vielen weltberühmten Autoren. Hörbücher kostenlos online hören oder herunterladen mit nur einem Klick.",
-            source: "https://horbuchkostenlos.de/",
-            image: "https://horbuchkostenlos.de/wp-content/uploads/2022/02/HorbuchKostenlos.png",
-            language: "de-de",
-            dateOfAdding: "2024-06-07",
-        },
-        {
-            sourceTitle: "Internet Archive",
-            title: "Audio Books & Poetry",
-            description:
-                "Listen to free audio books and poetry recordings! This library of audio books and poetry features digital recordings and MP3's from the Naropa Poetics Audio Archive, LibriVox, Project Gutenberg, Maria Lectrix, and Internet Archive users.",
-            source: "https://archive.org/details/audio_bookspoetry",
-            image: "https://archive.org/services/img/audio_bookspoetry",
-            language: "en-us",
-            dateOfAdding: "2024-06-07",
-        },
-    ];
+    const freeContentWebsitesExampleJson = "/freeContentWebSitesExample.json";
+    const freeContentWebsitesJson = "/web/freeContentWebSites.json";
+    const [sourceData, setSourceData] = useState<SourceData[]>([]);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [form] = Form.useForm();
+
+    useEffect(() => {
+        fetch(freeContentWebsitesJson)
+            .then((response) => response.json())
+            .then((data) => setSourceData(data))
+            .catch((error) => console.error("Error fetching data:", error));
+    }, []);
+
+    const showModal = () => {
+        form.resetFields();
+        setIsModalVisible(true);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+    const addEntryModal = (
+        <Modal
+            title={t("tonies.freeContent.addNewContent")}
+            open={isModalVisible}
+            onCancel={handleCancel}
+            okText={t("tonies.freeContent.form.save")}
+            onOk={() => {
+                form.validateFields()
+                    .then(() => {
+                        setIsModalVisible(false);
+                        setJsonData(JSON.stringify(form.getFieldsValue(), null, 2));
+                        setJsonViewerModalOpened(true);
+                    })
+                    .catch((info) => {
+                        console.log("Validate Failed:", info);
+                    });
+            }}
+        >
+            <Form form={form} layout="vertical">
+                <Form.Item
+                    name="sourceTitle"
+                    label={t("tonies.freeContent.form.sourceTitle")}
+                    rules={[{ required: true, message: t("tonies.freeContent.form.sourceTitleRequired") }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item name="title" label={t("tonies.freeContent.form.title")}>
+                    <Input />
+                </Form.Item>
+                <Form.Item name="description" label={t("tonies.freeContent.form.description")}>
+                    <Input.TextArea />
+                </Form.Item>
+                <Form.Item
+                    name="source"
+                    label={t("tonies.freeContent.form.source")}
+                    rules={[{ required: true, message: t("tonies.freeContent.form.sourceRequired") }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    name="image"
+                    label={t("tonies.freeContent.form.image")}
+                    rules={[{ required: true, message: t("tonies.freeContent.form.imageRequired") }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item name="language" label={t("tonies.freeContent.form.language")}>
+                    <Input />
+                </Form.Item>
+            </Form>
+        </Modal>
+    );
+
+    const [jsonData, setJsonData] = useState<string>("");
+    const [jsonViewerModalOpened, setJsonViewerModalOpened] = useState(false);
+
+    const jsonViewerModalFooter = (
+        <Button type="primary" onClick={() => setJsonViewerModalOpened(false)}>
+            {t("tonies.freeContent.form.ok")}
+        </Button>
+    );
+
+    const handleJsonViewerModalClose = () => {
+        setJsonViewerModalOpened(false);
+    };
+
+    function detectColorScheme() {
+        const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        const storedTheme = localStorage.getItem("theme");
+
+        if (storedTheme === "auto") {
+            return prefersDarkMode ? "dark" : "light";
+        } else {
+            return storedTheme;
+        }
+    }
+
+    const jsonViewerModal = (
+        <Modal
+            footer={jsonViewerModalFooter}
+            width={700}
+            title={t("tonies.freeContent.copy")}
+            open={jsonViewerModalOpened}
+            onCancel={handleJsonViewerModalClose}
+        >
+            {jsonData ? (
+                <>
+                    <Paragraph>{t("tonies.freeContent.form.copyHint")}</Paragraph>
+                    <SyntaxHighlighter
+                        language="json"
+                        style={detectColorScheme() === "dark" ? oneDark : oneLight}
+                        customStyle={{
+                            padding: 0,
+                            borderRadius: 0,
+                            margin: 0,
+                            border: "none",
+                        }}
+                    >
+                        {jsonData}
+                    </SyntaxHighlighter>
+                </>
+            ) : (
+                "Loading..."
+            )}
+        </Modal>
+    );
+
     return (
         <>
             <StyledSider>
@@ -91,65 +181,114 @@ export const FreeContentPage = () => {
                 />
                 <StyledContent>
                     <h1>{t(`tonies.freeContent.title`)}</h1>
-                    <Paragraph>{t("tonies.freeContent.text")}</Paragraph>
-                    <Paragraph>
-                        <List
-                            grid={{
-                                gutter: 16,
-                                xs: 1,
-                                sm: 2,
-                                md: 2,
-                                lg: 3,
-                                xl: 4,
-                                xxl: 6,
-                            }}
-                            dataSource={sourceArray}
-                            renderItem={(source) => (
-                                <List.Item key={source.sourceTitle}>
-                                    <Card
-                                        style={{
-                                            background: token.colorBgContainerDisabled,
-                                        }}
-                                        size="small"
-                                        title={
-                                            <div
+                    {addEntryModal}
+                    {jsonViewerModal}
+                    {sourceData.length === 0 ? (
+                        <>
+                            <Paragraph>
+                                <Trans
+                                    i18nKey="tonies.freeContent.textNoJson"
+                                    components={{
+                                        exampleJsonLink: (
+                                            <Link
+                                                to={freeContentWebsitesExampleJson.replace("/web/", "/")}
+                                                target="_blank"
+                                            />
+                                        ),
+                                    }}
+                                />
+                            </Paragraph>
+                            <Paragraph>
+                                <Button icon={<PlusOutlined />} onClick={showModal}>
+                                    {t("tonies.freeContent.addNewContent")}
+                                </Button>
+                            </Paragraph>
+                        </>
+                    ) : (
+                        <>
+                            <Paragraph>
+                                <Trans
+                                    i18nKey="tonies.freeContent.text"
+                                    components={{
+                                        exampleJsonLink: (
+                                            <Link to={freeContentWebsitesJson.replace("/web/", "/")} target="_blank" />
+                                        ),
+                                    }}
+                                />
+                            </Paragraph>
+                            <Paragraph>
+                                <Button icon={<PlusOutlined />} onClick={showModal}>
+                                    {t("tonies.freeContent.addNewContent")}
+                                </Button>
+                            </Paragraph>
+                            <Paragraph>
+                                <List
+                                    grid={{
+                                        gutter: 16,
+                                        xs: 1,
+                                        sm: 2,
+                                        md: 2,
+                                        lg: 3,
+                                        xl: 4,
+                                        xxl: 6,
+                                    }}
+                                    dataSource={sourceData}
+                                    renderItem={(source) => (
+                                        <List.Item key={source.sourceTitle}>
+                                            <Card
                                                 style={{
-                                                    display: "flex",
-                                                    justifyContent: "space-between",
-                                                    alignItems: "center",
+                                                    background: token.colorBgContainerDisabled,
                                                 }}
+                                                size="small"
+                                                title={
+                                                    <div
+                                                        style={{
+                                                            display: "flex",
+                                                            justifyContent: "space-between",
+                                                            alignItems: "center",
+                                                        }}
+                                                    >
+                                                        <div style={{ overflow: "hidden", textOverflow: "ellipsis" }}>
+                                                            {source.sourceTitle}
+                                                        </div>
+                                                        <Tooltip
+                                                            placement="top"
+                                                            zIndex={2}
+                                                            title={t("languageUtil." + source.language)}
+                                                        >
+                                                            <Text style={{ height: 20, width: "auto" }}>
+                                                                <LanguageFlagSVG
+                                                                    countryCode={source.language}
+                                                                    height={20}
+                                                                />
+                                                            </Text>
+                                                        </Tooltip>
+                                                    </div>
+                                                }
+                                                cover={
+                                                    <img
+                                                        alt={source.sourceTitle}
+                                                        src={source.image}
+                                                        style={{ padding: 8 }}
+                                                    />
+                                                }
+                                                actions={[
+                                                    <>
+                                                        <Link to={source.source} target="_blank">
+                                                            {t("tonies.freeContent.toExternalFreeContent")}{" "}
+                                                            <LinkOutlined />
+                                                        </Link>
+                                                    </>,
+                                                ]}
                                             >
-                                                <div style={{ overflow: "hidden", textOverflow: "ellipsis" }}>
-                                                    {source.sourceTitle}
-                                                </div>
-                                                <Tooltip
-                                                    placement="top"
-                                                    zIndex={2}
-                                                    title={t("languageUtil." + source.language)}
-                                                >
-                                                    <Text style={{ height: 20, width: "auto" }}>
-                                                        <LanguageFlagSVG countryCode={source.language} height={20} />
-                                                    </Text>
-                                                </Tooltip>
-                                            </div>
-                                        }
-                                        cover={
-                                            <img alt={source.sourceTitle} src={source.image} style={{ padding: 8 }} />
-                                        }
-                                        actions={[
-                                            <>
-                                                <Link to={source.source} target="_blank">
-                                                    {t("tonies.freeContent.toExternalFreeContent")} <LinkOutlined />
-                                                </Link>
-                                            </>,
-                                        ]}
-                                    >
-                                        <Card.Meta title={source.title} description={source.description} />
-                                    </Card>
-                                </List.Item>
-                            )}
-                        />
-                    </Paragraph>
+                                                <Card.Meta title={source.title} description={source.description} />
+                                            </Card>
+                                        </List.Item>
+                                    )}
+                                />
+                            </Paragraph>
+                        </>
+                    )}
                 </StyledContent>
             </StyledLayout>
         </>
